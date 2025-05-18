febbd9a681b3f1d0d872afd7ddcc381834529a55
Roman Kryvko <roman.kryvko@nure.ua>
2025-05-18 19:39:18 +0200

    Finish login / sign up page UI.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,46 @@
+import { useState } from "react"
+import '../assets/login.css'
+
 export default function Login() {
-  return <h1>Login Page</h1>
+  const [activeForm, setActiveForm] = useState('login')
+
+  return (
+    <div className="form-stack">
+      <div className="card-wrapper signup"
+        onClick={() => setActiveForm('signup')}
+      >
+        <div className={`card-tab signup-tab ${activeForm === 'signup' ? 'active' : ''}`}>
+          <p className="noselect">Sign up</p>
+        </div>
+        <div className={`form-card signup-card ${activeForm === 'signup' ? 'active' : ''}`}>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <div className="button-group">
+            <button>Google</button>
+            <button>Facebook</button>
+          </div>
+          <button className="main-button">Sign up</button>
+        </div>
+      </div>
+
+      <div
+        className="card-wrapper login"
+        onClick={() => setActiveForm('login')}
+      >
+        <div className={`card-tab login-tab ${activeForm === 'login' ? 'active' : ''}`}>
+          <p className="noselect">Login</p>
+        </div>
+        <div className={`form-card login-card ${activeForm === 'login' ? 'active' : ''}`}>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <div className="button-group">
+            <button>Google</button>
+            <button>Facebook</button>
+          </div>
+          <button className="main-button">Login</button>
+        </div>
+      </div>
+    </div >
+  )
 }
